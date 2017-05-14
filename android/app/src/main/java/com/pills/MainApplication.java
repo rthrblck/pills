b83422d3c857f5e361d24879545eeed58017a182
alan bount <alan@zeroasterisk.com>
2017-05-14 03:46:13 -0400

    adding camera plugin, untested

--- android/app/src/main/java/com/pills/MainApplication.java
+++ android/app/src/main/java/com/pills/MainApplication.java
@@ -3,6 +3,7 @@ package com.pills;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.microsoft.codepush.react.CodePush;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTCameraPackage(),
             new CodePush(getResources().getString(R.string.reactNativeCodePush_androidDeploymentKey), getApplicationContext(), BuildConfig.DEBUG)
       );
     }
